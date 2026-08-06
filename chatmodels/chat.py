08c246f694687d160d3431f4448063e6f0805a45
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-08-06 18:50:43 +0530

    “new”

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain.chat_models import init_chat_model
+from langchain_groq import ChatGroq
 
-model = init_chat_model("gpt-4.1")
+model = ChatGroq(
+    model="llama-3.3-70b-versatile"
+)
 
 response = model.invoke("Hello! Tell me a fun fact.")
 
