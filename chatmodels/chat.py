9b782c14bdf92349f520caf854c89a217dabec08
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-08-06 18:39:26 +0530

    “new”

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -1,3 +1,7 @@
 from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
+
+from langchain.chat_models import init_chat_model
+
+init_chat_model("gpt-4.1")  # Initialize the chat model
