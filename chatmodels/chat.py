16efe540ec09ccd97962fb8d4181b786e3249a9a
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-08-07 14:25:23 +0530

    “new”

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -1,14 +1,18 @@
 import os
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "hf_xxxxxxxxxxxxxxxxx"
+from urllib import response
 
 from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "hf_..."
 
 llm=HuggingFaceEndpoint(
-    repo_id="deepseek-ai/DeepSeek-R1",
-    temperature=0.7,
+    repo_id="deepseek-ai/DeepSeek-R1-0528",
+    temperature=0.7
 )
 model=ChatHuggingFace(llm=llm)
+
+response=model.invoke("What is the capital of France?")
+print(response.content)
