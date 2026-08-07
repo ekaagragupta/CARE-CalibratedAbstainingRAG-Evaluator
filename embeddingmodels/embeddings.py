a8d2d89cd514fc532cc5e2758f1ae4d0ed7d8c24
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-08-07 16:34:18 +0530

    “new”

--- embeddingmodels/embeddings.py
+++ embeddingmodels/embeddings.py
@@ -1,9 +1,18 @@
 from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+load_dotenv()
 
 embeddings=OpenAIEmbeddings(
-    models="text-embedding-3-large",
-    OPENAI_API_KEY="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",
-    dimension=30
+    model="text-embedding-3-large",
+    dimension=64
 )
-vector=embeddings.embed_query("What is the capital of india?")
+
+texts=[
+    "hello world",
+    "What is the capital of India?",
+    "What is the capital of France?"
+]
+
+
+vector=embeddings.embed_documents(texts)
 print(vector)
---
+++ embeddingmodels/huggingface_embeddings.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+
+HuggingFaceEmbeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={"device": "cpu"},
+)
+
+text=[
+    "hello world",
+    "What is the capital of India?",
+    "What is the capital of France?"
+]
+
+vector=HuggingFaceEmbeddings.embed_documents(text)
+print(len(vector))
