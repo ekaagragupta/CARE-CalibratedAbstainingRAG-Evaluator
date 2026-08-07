2a433acf84dcae227d7019683436325ea9216692
Ekaagra Gupta <ekaagrag2006@gmail.com>
2026-08-07 17:38:56 +0530

    “new”

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -1,26 +1,34 @@
 import os
-os.environ["HUGGINGFACE_API_TOKEN"] = "hf_xxxxxxxxxxxxxxxxx"
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
-from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
-from langchain_core import AIMessage, HumanMessage, SystemMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
-llm=HuggingFaceEndpoint(
+llm = HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-R1-0528",
-    temperature=0.7
+    temperature=0.7,
 )
-model=ChatHuggingFace(llm=llm)
-messages=[
-    SystemMessage(content="You are a genz assistant.")
+
+model = ChatHuggingFace(llm=llm)
+
+messages = [
+    SystemMessage(content="You are a Gen Z assistant.")
 ]
+
+print("--------- Welcome to Chat Bot ---------")
+
 while True:
-    print("---------welcome to chat bot---------")
-    prompt=input("you:")
-    messages.append(HumanMessage(content=prompt))
-    if(prompt=="0"):
+    prompt = input("You: ")
+
+    if prompt == "0":
         break
-    response=model.invoke(messages)
+
+    messages.append(HumanMessage(content=prompt))
+
+    response = model.invoke(messages)
+
     messages.append(AIMessage(content=response.content))
-    print("bot",response.content)
+
+    print("Bot:", response.content)
